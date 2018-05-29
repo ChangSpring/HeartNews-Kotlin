65de5e0f3d0a7daf258bfd9c6b2d09c96dfcabc1
ChangSpring <ChangSpring0728@gmail.com>
2018-05-29 10:55:08 +0800

    test

--- app/src/main/java/com/alfred/heartnews/ui/viewmodel/OnRequestListener.kt
+++ app/src/main/java/com/alfred/heartnews/ui/viewmodel/OnRequestListener.kt
@@ -4,9 +4,8 @@ package com.alfred.heartnews.ui.viewmodel
  * Created by alfred on 2018/5/23.
  */
 interface OnRequestListener<in T> {
-    
+
     fun onSuccess(t: T?)
 
     fun onFailure(message : String?)
-
 }
